7b9259d62cb486cbbcea0f59756acebeee335a69
Phoomss <narongsak5az@gmail.com>
2025-11-30 21:55:37 +0700

    fix css class add cursor pointer of text scale

--- frontend/src/components/shared/TextScaleButton.jsx
+++ frontend/src/components/shared/TextScaleButton.jsx
@@ -9,21 +9,21 @@ const TextScaleButton = () => {
 
       <button 
         onClick={() => setScale('small')}
-        className={`${scale === 'small' ? 'font-bold underline' : ''}`}
+        className={`${scale === 'small' ? 'font-bold underline' : ''} cursor-pointer`}
       >
         <span className="text-xs">A</span>
       </button>
 
       <button 
         onClick={() => setScale('normal')}
-        className={`${scale === 'normal' ? 'font-bold underline' : ''}`}
+        className={`${scale === 'normal' ? 'font-bold underline' : ''} cursor-pointer`}
       >
         <span className="text-sm">A</span>
       </button>
 
       <button 
         onClick={() => setScale('large')}
-        className={`${scale === 'large' ? 'font-bold underline' : ''}`}
+        className={`${scale === 'large' ? 'font-bold underline' : ''} cursor-pointer`}
       >
         <span className="text-lg">A</span>
       </button>
